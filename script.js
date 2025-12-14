2b8457c0b44c31f0409c4a57d8e3998c4d35049a
QalandarovShokhjahon <shokhjaxonqalandarov@gmail.com>
2025-12-14 17:32:46 +0500

    initial

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// **`Delegatsiya`** → bu **bitta umumiy ota elementga** hodisa (`event`) qo‘shish orqali uning **barcha farzand elementlariga** ta’sir qilish usulidir.
+
+// Agar Delegatsiyasiz har bir elementga alohida hodisa yozilsa, kod ko‘payadi, xotira ko‘proq sarflanadi va yangi element qo‘shilganda yana qo‘shimcha kod yozish kerak bo‘ladi — shu sababli delegatsiya samaraliroq usul hisoblanadi.
